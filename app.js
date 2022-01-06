79b27234261bd751eda1e60174d2392371a663bb
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-01-05 21:18:08 -0600

    trying to get git

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-
-
-
-
-const printProfileData = profileDataArr => {
-    // This...
-    for(let i = 0; i <profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('===============');
-
-    // Is the same as this...
-    profileDataArr.foreach(profileItem => console.log(profileItem));
-       
-    };
-    
-
-
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+const generatePage = (userName, githubName) => {
+  return `
+      name: ${userName} 
+      github: ${githubName}
+    `;
+};
+console.log(generatePage(name, github));
