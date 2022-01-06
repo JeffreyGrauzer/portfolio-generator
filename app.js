ed63b03410f7dbe77ae78d2e25c937a442a800e1
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-01-06 13:34:55 -0600

    modularized code

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
-const generatePage = (userName, githubName) => {
-  return `
-      name: ${userName} 
-      github: ${githubName}
-    `;
-};
-console.log(generatePage(name, github));
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
